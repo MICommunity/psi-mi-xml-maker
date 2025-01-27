6e019bfb3c5b512d02c9b92e910a6e4bb057b320
Susie <hugetsusie@gmail.com>
2025-01-27 10:27:40 +0000

    Java packager for mac, windows, linux

--- src/main/java/uk/ac/ebi/intact/psi/mi/xmlmaker/file/processing/content/DataForRawFile.java
+++ src/main/java/uk/ac/ebi/intact/psi/mi/xmlmaker/file/processing/content/DataForRawFile.java
@@ -1,15 +1,11 @@
 package uk.ac.ebi.intact.psi.mi.xmlmaker.file.processing.content;
 
-import java.util.Objects;
-
 public enum DataForRawFile {
     INTERACTION_DETECTION_METHOD("Interaction Detection Method", false, true),
     PARTICIPANT_DETECTION_METHOD("Participant Detection Method", false, true),
     EXPERIMENTAL_PREPARATION("Experimental Preparation", false, true),
     BAIT_BIOLOGICAL_ROLE("Bait Biological Role", true, false),
-//    BAIT_EXPERIMENTAL_ROLE("Bait Experimental Role", true, false),
     PREY_BIOLOGICAL_ROLE("Prey Biological Role", false, false),
-//    PREY_EXPERIMENTAL_ROLE("Prey Experimental Role", false, false);
     ;
 
     public final String name;
